e1a0387a0f8dc673fc5f1f4275b46ae60362852c
Iarley Santos <iarleyconceicaodossantos@gmail.com>
2025-09-15 21:28:15 -0300

    initial commit

--- controller/src/main.cpp
+++ controller/src/main.cpp
@@ -1,6 +1,17 @@
 #include <Arduino.h>
 
-int cont = 0;
+
+typedef union
+{
+  float number;
+  uint8_t bytes[4];
+} DATA;
+
+void serial_write(float value);
+float get_float();
+
+DATA data;
+DATA dado;
 
 void setup()
 {
@@ -15,9 +26,41 @@ void setup()
 
 void loop() 
 {
-  cont++;
-  // Publica uma mensagem a cada segundo
-  Serial.println(cont);
+  dado.number = get_float();
+  if (dado.number == 1)
+  {
+    digitalWrite(2, 0);
+  }
 
   delay(100);
+
+  serial_write(dado.number);
+}
+
+// função para enviar um dado do tipo float para a serial
+void serial_write(float value)
+{
+  DATA a;
+  a.number = value;
+  Serial.write('A'); // para liberar a comunicação
+
+  for (int i=0; i<4; i++)
+  {
+    Serial.write(a.bytes[i]);
+  }
+
+  Serial.print('\n');
+}
+
+// funcao para receber dado da serial
+float get_float()
+{
+  int cont = 0;
+  DATA f;
+  while(cont<4)
+  {
+    f.bytes[cont] = Serial.read();
+    cont += 1;
+  }
+  return f.number;
 }
